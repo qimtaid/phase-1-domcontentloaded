e4cada52cf01c8abfda49a7a13a77fe0b6e93af1
Dan Koskei <qimtaid@gmail.com>
2024-04-01 11:21:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  
